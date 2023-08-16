0a68936704744c5284bbf6e212fa777603c2bf1f
iseriohn <jyamy42@gmail.com>
2023-08-16 09:38:17 -0400

    minor, update config

--- dapps/phase2-registration/src/routes/config.tsx
+++ dapps/phase2-registration/src/routes/config.tsx
@@ -3,5 +3,5 @@ export const numChunk = 3;
 export const mode = 'public_rehearsal';
 
 // export const URL = 'http://tsc-dev.sui.io/';
-// export const URL = 'https://record.sui-phase2-ceremony.iseriohn.com';
-export const URL = 'http://127.0.0.1:37681';
+export const URL = 'https://record.sui-phase2-ceremony.iseriohn.com';
+// export const URL = 'http://127.0.0.1:37681';
